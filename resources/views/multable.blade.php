86ca65b4fe7f2c5b9e82418082246c0b7056419a
MohamedMETA10 <155086605+MohamedMETA10@users.noreply.github.com>
2025-02-22 23:53:14 +0200

    Assigment 1

---
+++ resources/views/multable.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.master')
+@section('title', 'Prime Numbers')
+@section('content')
+@php($j = 5)
+
+    <div class="card m-4 col-sm-2">
+        <div class="card-header"> Multiplication Table of {{$j}}</div>
+        <div class="card-body">
+            <table>
+                @foreach (range(1, 10) as $i)
+                <tr>
+                    <td>{{$i}} * {{$j}}</td>
+                    <td> = {{ $i * $j }}</td>
+                    </li>
+                    @endforeach
+            </table>
+        </div>
+    </div>
+    @endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title', 'welcome')
+@section('content')
+
+<div class="card m-4">
+    <div class="card-body">
+        Welcome to Home Page
+    </div>
+</div>
+@endsection
